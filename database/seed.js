2663a7237ddd7da9f788db292b2036ba8641be14
ecki-d <support@kaizenmedia.co.za>
2019-07-24 17:42:20 +0200

    :wrench: Refactor seed code.

--- database/seed.js
+++ database/seed.js
@@ -1,26 +1,29 @@
 const Post = require("./models/posts");
 const scraper = require("../services/web-scraper");
 
+const updateData = async posts => {
+  const missingPosts = await scraper.update(posts);
+
+  if (missingPosts.length) {
+    for (let i = 0; i < missingPosts.length; i++) {
+      await Post.create(missingPosts[i]);
+    }
+  }
+
+  return true;
+};
+
+const seedData = async () => {
+  const seededPosts = await scraper.seed();
+
+  for (let i = seededPosts.length; i >= 0; i--) {
+    await Post.create(seededPosts[i]);
+  }
+
+  return true;
+};
+
 module.exports = async () => {
-  Post.find().then(async posts => {
-    if (posts.length > 0) {
-      const missingPosts = await scraper.update(posts);
-
-      if (missingPosts.length) {
-        for (let i = 0; i < missingPosts.length; i++) {
-          await Post.create(missingPosts[i]);
-        }
-      }
-
-      return true;
-    }
-
-    const seededPosts = await scraper.seed();
-
-    for (let i = seededPosts.length; i >= 0; i--) {
-      await Post.create(seededPosts[i]);
-    }
-
-    return true;
-  });
+  const posts = await Post.find();
+  return posts.length > 0 ? await updateData(posts) : await seedData();
 };

30cc502a18ce8ac53dcd1d99c5e4571e7fcd027e
ecki-d <support@kaizenmedia.co.za>
2019-07-24 07:20:56 +0200

    Get ALL posts and save to file. Start periodic check function. :sparkles: :wrench:

--- services/web-scraper.js
+++ services/web-scraper.js
@@ -1,5 +1,7 @@
 const cheerio = require("cheerio");
 const fetch = require("node-fetch");
+// const { writeFileSync, readFile } = require("fs");
+const { join } = require("path");
 const baseURL = "http://forumonline.unam.edu.na";
 const allPath = "/category/all-news";
 
@@ -12,75 +14,83 @@ const getHTML = async url => {
 };
 
 const getFullContent = async url => {
-  getHTML(url).then(html => {
-    let $ = cheerio.load(html);
-    const full_text = $(".entry-content").text();
-    const full_html = $(".entry-content").html();
+  try {
+    let html = await getHTML(url);
+    let $ = await cheerio.load(html);
 
-    console.log(full_text);
+    let full_text = await $(".entry-content")
+      .first()
+      .text();
+    let full_html = await $(".entry-content")
+      .first()
+      .html();
 
-    return {
-      full_text,
-      full_html,
-    };
-  });
+    return { full_html, full_text };
+  } catch (e) {
+    return e;
+  }
 };
 
-const parseCurrentPagePosts = async () => {
-  getHTML(baseURL + allPath).then(html => {
-    let $ = cheerio.load(html);
+const parseCurrentPagePosts = async url => {
+  const html = await getHTML(url);
+  let $ = cheerio.load(html);
+  let postArr = $(".post");
+  let len = postArr.length;
 
-    $(".post").each(async (i, el) => {
-      let id = $(el)[0].attribs.id;
+  for (let i = 0; i < len; i++) {
+    const el = $(postArr[i]);
 
-      let post_url = $(el)
-        .find($(".thumbnail-link"))
-        .first()
-        .attr("href");
+    let id = await el[0].attribs.id;
 
-      let title = $(el)
-        .find($(".entry-title > a"))
-        .first()
-        .text();
+    let post_url = await el
+      .find($(".thumbnail-link"))
+      .first()
+      .attr("href");
 
-      let author = $(el)
-        .find($(".entry-author a[rel='author']"))
-        .first()
-        .text();
+    let title = await el
+      .find($(".entry-title > a"))
+      .first()
+      .text();
 
-      let summary = $(el)
-        .find($(".entry-summary"))
-        .first()
-        .text();
+    let author = await el
+      .find($(".entry-author a[rel='author']"))
+      .first()
+      .text();
 
-      let created = $(el)
-        .find($(".entry-date"))
-        .first()
-        .text();
+    let summary = await el
+      .find($(".entry-summary"))
+      .first()
+      .text();
 
-      let image = $(el)
-        .find(".thumbnail-wrap img")
-        .first()
-        .attr("src")
-        .replace("-300x200", "");
+    let created = await el
+      .find($(".entry-date"))
+      .first()
+      .text();
 
-      // let page_content = await getFullContent(post_url);
+    let image = await el
+      .find(".thumbnail-wrap img")
+      .first()
+      .attr("src")
+      .replace("-300x200", "");
 
-      // let { full_text, full_html } = page_content;
+    let page_content = await getFullContent(post_url);
 
-      return {
-        id,
-        post_url,
-        title,
-        author,
-        summary,
-        created,
-        image,
-        // full_text,
-        // full_html,
-      };
+    let { full_text, full_html } = page_content;
+
+    posts.push({
+      id,
+      post_url,
+      title,
+      author,
+      summary,
+      created,
+      image,
+      full_text,
+      full_html,
     });
-  });
+  }
+
+  return posts;
 };
 
 const requestOK = async url => {
@@ -95,23 +105,50 @@ const requestOK = async url => {
 
 const scrapeRecursive = async () => {
   let currentPage = 1;
-  const paginationURL = baseURL + "/page/";
+  let paginationURL = baseURL + allPath + "/page/";
 
   while (true) {
-    const ok = await requestOK(paginationURL + currentPage);
+    let ok = await requestOK(paginationURL + currentPage);
+
     if (!ok) break;
 
-    const post = await parseCurrentPagePosts();
-    posts.push(post);
+    await parseCurrentPagePosts(paginationURL + currentPage);
 
     currentPage++;
   }
-
   return posts;
 };
 
-scrapeRecursive()
-  .then(() => {
-    console.log(posts);
-  })
-  .catch(e => console.error(e));
+const checkIfUpdated = async () => {
+  let url = baseURL + allPath + "/page/1";
+  let parsed = await parseCurrentPagePosts(url);
+
+  readFile(join(__dirname, "../data/posts.json"), (err, data) => {
+    if (err) return false;
+    let saved = JSON.parse(data);
+
+    if (parsed[0].id === saved[0].id) {
+      return true;
+    }
+
+    // TODO: implement adding stuff
+
+    return false;
+  });
+};
+
+checkIfUpdated()
+  .then(res => res)
+  .catch(e => e);
+
+// scrapeRecursive()
+//   .then(response => {
+//     writeFileSync(
+//       join(__dirname, "../data/posts.json"),
+//       JSON.stringify(response),
+//       {
+//         encoding: "utf8",
+//       }
+//     );
+//   })
+//   .catch(e => console.error(e));

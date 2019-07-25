f905dd03fe1714cf78f781e3ba52ac350ab14b13
ecki-d <support@kaizenmedia.co.za>
2019-07-25 16:00:38 +0200

    :sparkles: Parse count and start params on GET /posts (incl. defaults)

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,26 @@
-const Post = require("../database/models/posts");
 const { Router } = require("express");
-
 const router = new Router();
 
-router.get("/", async (_, res) => {
+const Post = require("../database/models/posts");
+const { setStart, setCount } = require("../services/query-handlers");
+
+router.get("/", async (req, res) => {
   const posts = await Post.find();
-  let data, start, count, categories;
-  res.json(posts.reverse());
+  let { start = 0, count = 20, category, author, from, to } = req.query;
+  let response = {
+    data: [""],
+    start,
+    count,
+    from: new Date(),
+    to: new Date(),
+  };
+
+  start = setStart(start, posts.length);
+  count = setCount(start, count, posts.length);
+
+  response.data = posts.filter((_, index) => index >= start && index < start + count).reverse();
+
+  res.json({ from, to, ...response });
 });
 
 module.exports = router;
---
+++ services/query-handlers.js
@@ -0,0 +1,43 @@
+function setCount(start, count, length) {
+  start = cleanNumber(start) || 0;
+  count = cleanNumber(count) || 20;
+  
+  if(count > 20) {
+    count = 20;
+  }
+
+  if(count < 1) {
+    count = 1;
+  }
+
+  if(start + count > length -1) {
+    count = (length - 1) - start;
+  }
+
+  return parseInt(count);
+}
+
+function setStart(start, length) {
+  if(start > length -1) {
+    start = length;
+  }
+
+  if(start < 0) {
+    start = 0;
+  }
+
+  return parseInt(start);
+}
+
+function cleanNumber(param) {
+  try {
+    return parseInt(param);
+  } catch {
+    return undefined;
+  }
+}
+
+module.exports = {
+  setCount,
+  setStart
+}
